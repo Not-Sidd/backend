9542e4d1ac4677f7ad5a20717e71aa3df30f119a
Not-Sidd <siddhartha2647@gmail.com>
2026-04-08 16:29:32 +1000

    database added

---
+++ src/main/java/com/marketplace/backend/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.marketplace.backend.controller;
+
+
+
+import com.marketplace.backend.model.User;
+import com.marketplace.backend.respository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userRepository.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+}
